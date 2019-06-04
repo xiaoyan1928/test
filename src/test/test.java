ffe5d09564dd3792480f4031a51a9180591a0ae6
Administrator <277747546@qq.com>
2019-06-04 21:11:14 +0800

    test2

--- src/test/test.java
+++ src/test/test.java
@@ -4,5 +4,6 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("tijiao daima");
 		System.out.println("just soso");
+		System.out.println("just soso so ");
 	}
 }
